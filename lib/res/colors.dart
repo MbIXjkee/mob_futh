b8c6a415be3797d27bcdfb3c0939d5c03fccaa25
zotyev <zotev@surfstudio.ru>
2020-09-02 23:03:12 +0300

    Shadow example start.

---
+++ lib/res/colors.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = _nightRider,
+    primaryColor = _nero,
+    headColor = _eclipse,
+    shadowMainColor = _nero,
+    shadowTintColor = _matterhorn,
+    timerTextColor = _darkGray,
+    timerDividerColor = _dimGray,
+    buttonDisabledColor = _midnightExpress,
+    buttonEnabledColor = _lavenderBlue,
+    buttonBulbColor = _white;
+
+const Color _nightRider = Color(0xFF323232),
+    _eclipse = Color(0xFF373737),
+    _nero = Color(0xFF242424),
+    _matterhorn = Color(0xFF505050),
+    _dimGray = Color(0xFF727272),
+    _darkGray = Color(0xFFA4A4A4),
+    _midnightExpress = Color(0xFF1B202C),
+    _lavenderBlue = Color(0xFFB9CDFF),
+    _white = Colors.white;
